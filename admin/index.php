7453e4ab44d069d527b79548e1cb068549e8268b
Leandro Coelho <leandromendoncacoelho@gmail.com>
2018-01-23 10:52:29 -0200

    implementação index e tela de login

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../assets/img/logo.png" width="140" height="140">
+
+  <title>Ideia10</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="../assets/css/carousel.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="offset-md-4 col-md-4">
+          <form class="form-signin">
+            <div class="text-center">
+              <img src="../assets/img/logo.png" alt="" width="65%">
+            </div>
+            <h1 class="h3 font-weight-normal text-center">Login</h1>
+            <label for="inputEmail" class="sr-only">E-mail</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required="" autofocus="">
+            <label for="inputPassword" class="sr-only">Senha</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required="">
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" value="remember-me"> Lembrar Senha
+              </label>
+            </div>
+            <button class="btn btn-lg btn-verde btn-block" type="submit">Entrar</button>
+            <p class="text-muted text-center"><br>© 2017-2018</p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/bower_components/jquery/dist/jquery.slim.min.js"></script>
+  <script src="../assets/bower_components/popper.js/dist/umd/popper.min.js"></script>
+  <script src="../assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="../assets/bower_components/holderjs/holder.min.js"></script>
+  <script src="../assets/bower_components/instafeed/instafeed.min.js"></script>
+</body>
+</html>
